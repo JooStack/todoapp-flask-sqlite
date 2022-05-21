50e988e46c368b6d1d5acf7476e51b2d2fde0488
JooStack <joostack@gmail.com>
2022-05-21 17:29:29 +0300

    created

--- todo.py
+++ todo.py
@@ -8,7 +8,17 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index() :
-    return render_template("index.html")
+    todos = Todo.query.all()
+    return render_template("index.html", todos = todos)
+
+
+@app.route("/complete/<string:id>")
+def completeTodo(id) :
+    todo = Todo.query.filter_by(id=id).first()
+    
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
 
 @app.route("/add",methods = ["POST"])
 def addTodo() :
@@ -17,6 +27,13 @@ def addTodo() :
     db.session.add(newTodo)
     db.session.commit()
 
+    return redirect(url_for("index"))
+
+@app.route("/delete/<string:id>")
+def deleteTodo(id) :
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
     return redirect(url_for("index"))
 
 class Todo(db.Model) :
